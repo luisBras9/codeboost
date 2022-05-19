85f8a6b0b2903ae9032f454f9dc448c66bb2e8c1
Matheus Costa <matheuscostadosantos@hotmail.com>
2022-05-19 12:54:43 -0300

    Aula variaveis

--- javascript/js/scripts.js
+++ javascript/js/scripts.js
@@ -1,3 +1,43 @@
-console.log("Ola Javascript!");
+//console.log("Ola Javascript!");
 
-alert("Alert do Javascript!");
+//alert("Alert do Javascript!");
+
+/* 
+  Variáveis
+  - Responsaveis por guardar dados na memoria
+*/
+
+// Declaração de variaveis
+var marcaCarro = "Chevrolet";
+var modeloCarro = "Onix";
+var corCarro = "Preto";
+var anoCarro = "2018";
+var precoCarro = 40.0;
+var carroVendido = false;
+
+// console.log(marcaCarro + " - " + modeloCarro);
+
+/* 
+  Tipos de Variáveis
+  - var
+  - let
+  - const
+*/
+
+// var (Pode ser alterada após execução)
+var nome = "Matheus";
+var nome = "Matheus Costa";
+
+//  const e let (Não podem ser alterada!!!)
+const sobrenome = "Costa";
+
+/* 
+  Operações 
+*/
+
+let valor01 = 5;
+let valor02 = 15;
+
+let resultado = valor01 + valor02;
+
+console.log(resultado);
