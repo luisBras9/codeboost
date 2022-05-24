f70a052074799e02b7609e560ae9a88a0f0d7be3
Matheus Costa <matheuscostadosantos@hotmail.com>
2022-05-24 12:39:21 -0300

    Aula tipo de dados

--- javascript/js/scripts.js
+++ javascript/js/scripts.js
@@ -41,3 +41,40 @@ let valor02 = 15;
 let resultado = valor01 + valor02;
 
 console.log(resultado);
+
+/* 
+  Tipos de Dados
+*/
+
+var marcaCarro = "Gol" // String
+var anoCarro = 2021 // Number
+var carroVendido = true // Boolean
+var qtdRodas // Undefined
+var carroAutomatico = null // Null
+var simbolo = Symbol() // Symbol
+var objetoCarro = {} // Object
+
+var propriedadesCarro = [
+  {
+    marca: "Chevrolet",
+    modelo: "Onix" ,
+    cor: "Preto"
+  },
+  {
+    marca: "Volkswagem",
+    modelo: "Gol" ,
+    cor: "Prata"
+  }
+];
+
+// Acessando a propriedade do objeto/array
+console.log(propriedadesCarro[0].marca);
+
+// Typeof - função para identificar o tipo de dado da variavel
+console.log(typeof propriedadesCarro);
+
+// Length - função para somar o total
+console.log(propriedadesCarro.length);
+
+// Template Strings (concatenação de variáveis)
+console.log(`Marca do carro: ${marcaCarro} - Ano de fabricação: ${anoCarro}`)
